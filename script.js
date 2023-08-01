ca83900cd84f45150955e7f4d70c7bba011e56f8
otmorales23 <oliverwills0227@gmail.com>
2023-07-31 18:42:42 -0700

    added array of questions and choices

---
+++ script.js
@@ -0,0 +1,23 @@
+//create array of questions
+var questionArrr = [
+    { question: "JavaScript is a ___ -side programming language.",
+    choices: ["Client", "Server", "Both"]
+},
+{   question: "Which of the following keyords is used to define a variable in Javascript?",
+choices: ["var", "const", "char"]
+},
+
+{   question: "What does HTML stand for?",
+choices: ["Hyperlink Text Manual Letters", "How To Make Latkes", "Hypertext Markup Language"]
+},
+
+{   question: "What does CSS stand for?",
+choices: ["Colorful Style Sheets", "Cascading Style Sheets", "Can't Stand Supper"]
+},
+
+{   question: "Which is the correct HTML element for the largest heading?",
+choices: ["<h4>", "<h372>", "<h1>"]
+},
+]
+
+//create button that begins quiz
