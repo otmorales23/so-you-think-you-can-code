c00a6ca06fc75d8ca9864a1a5c0c8a5663eeccda
otmorales23 <oliverwills0227@gmail.com>
2023-07-31 18:59:09 -0700

    added countdown timer for quiz

--- script.js
+++ script.js
@@ -1,23 +1,49 @@
+let startBtn = document.getElementById("startBtn")
+let timer = document.getElementById("timer")
+
 //create array of questions
 var questionArrr = [
-    { question: "JavaScript is a ___ -side programming language.",
-    choices: ["Client", "Server", "Both"]
-},
-{   question: "Which of the following keyords is used to define a variable in Javascript?",
-choices: ["var", "const", "char"]
-},
+    {
+        question: "JavaScript is a ___ -side programming language.",
+        choices: ["Client", "Server", "Both"]
+    },
+    {
+        question: "Which of the following keyords is used to define a variable in Javascript?",
+        choices: ["var", "const", "char"]
+    },
 
-{   question: "What does HTML stand for?",
-choices: ["Hyperlink Text Manual Letters", "How To Make Latkes", "Hypertext Markup Language"]
-},
+    {
+        question: "What does HTML stand for?",
+        choices: ["Hyperlink Text Manual Letters", "How To Make Latkes", "Hypertext Markup Language"]
+    },
 
-{   question: "What does CSS stand for?",
-choices: ["Colorful Style Sheets", "Cascading Style Sheets", "Can't Stand Supper"]
-},
+    {
+        question: "What does CSS stand for?",
+        choices: ["Colorful Style Sheets", "Cascading Style Sheets", "Can't Stand Supper"]
+    },
 
-{   question: "Which is the correct HTML element for the largest heading?",
-choices: ["<h4>", "<h372>", "<h1>"]
-},
+    {
+        question: "Which is the correct HTML element for the largest heading?",
+        choices: ["<h4>", "<h372>", "<h1>"]
+    },
 ]
 
 //create button that begins quiz
+startBtn.addEventListener("click", function () {
+    startBtn.style.display = "none"
+    startTimer()
+})
+
+//create timer and start quiz
+function startTimer() {
+    let timerCount = 10
+    timer.textContent = timerCount;
+    let countdown = setInterval(() => {
+        timerCount--
+        timer.textContent = timerCount;
+        if (timerCount <= 0) {
+            clearIntveral(countdown)
+            timer.style.display = "none";
+        }
+    }, 1000);
+}
